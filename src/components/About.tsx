803bf250579d26822f52d9cb03952b4ece4eab0c
rrk0804 <rajitkhtr@gmail.com>
2025-05-18 20:49:56 -0400

    Updated the about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,41 +4,37 @@ function About() {
             <section className="about-description">
                 <p>
                     One of my most distinct memories of high school is designing a website for a family business using HTML, CSS, and JavaScript. During
-                    the summer between my senior year of high school and freshman year at college, I needed a summer job. An exciting opportunity came to me when a close friend
-                    said that he was looking for someone to design a website for his family's restaurant. I decided to give the challenge a shot. During this 3-month stint, I 
-                    grew from knowing very little about programming to delivering a website that anybody with Internet access could use to view the restaurant menu, learn about
-                    the restaurant's history, and leave reviews. I loved the process of writing code incrementally and interacting with the results on a web browser. I also 
-                    liked the huge open source community of web programmers and used many of their creations to decrease development time. After this summer, I knew that I wanted
-                    to do something related to programming and computers in my future career. So, I ended up choosing computer engineering as my major in college.
+                    the months between the end of senior year of high school and beginning of freshman year at college, I needed a summer job. An exciting opportunity came 
+                    to me when a close friend  said that he was looking for someone to design a website for his family's restaurant. I decided to give the challenge a shot. 
+                    Over three months, I grew from knowing very little about programming to delivering a website that anybody with Internet access could use to view 
+                    the restaurant menu, learn about the restaurant's history, and leave reviews. I loved the process of writing code incrementally and interacting with the results 
+                    on a web browser. I also liked the huge open source community of web programmers and used many of their creations in the projecy. After this summer, I knew that 
+                    I wanted to do something related to programming and computers in my future career. So, I ended up choosing computer engineering as my major in college.
                 </p>
                 <p>
-                    I am a co-creator of <a href="https://github.com/ECE49595-Team-6/EnhancifyInstall" className="about-link">Enhancify</a>, an open source Spotify extension.
+                    At <a href="https://www.purdue.edu" className="about-link">Purdue University</a>, I developed fundamental skills in computer engineering: programming, 
+                    debugging/troubleshooting, software engineering, data science,
+                    hardware engineering, and embedded systems. I also took electives to learn and practice soft skills like problem solving, communication, presentation, business writing,
+                    teamwork, and project management. In addition to classes, I was actively involved in many student organizations. During my sophomore year, I became a team leader (TL)
+                    for Purdue's orientation program Boiler Gold Rush (BGR). In the span of a week, I was responsible for familiarizing students with the campus, providing them information
+                    about classes and clubs, and making sure they had everything they needed to ace their freshman year. My excellent team leaders during during my orientation at Purdue inspired 
+                    me to join the orientations committee and provide the same experience to incoming freshmen. Other things I was involved in at Purdue are academic research, teaching 
+                    assistantships, and Purdue IEEE. 
                 </p>
                 <p>
-                    Previously, I was employed at <a href="https://www.elevancehealth.com" className="about-link">Elevance Health</a> and <a href="https://www.purdue.edu" className="about-link">Purdue University</a>.
+                   My career in I.T. began at <a href="https://www.elevancehealth.com" className="about-link">Elevance Health</a>, a large health insurance company. In my information technology (I.T)
+                   internship at Elevance Health, I analyzed the company's I.T. incidents data to find recurring incidents and patterns. I successfully identified the top 5 recurring incidents, held
+                   root cause analysis (RCA) meetings with subject matter experts, and proposed preventative recommendations to permanently resolve the recurring incidents. I also created a dashboard
+                   for incident management to visualize metrics like mean time to resolution (MTTR), SLA breaches, and number of unresolved incidents to help the company assess the performance of
+                   the incident management team. This internship was very rewarding because it exposed me to enterprise I.T. and helped me decide what career I wanted after graduating from college. During
+                   the next summer, I was invited back as a software engineering intern. I helped the company build an application to manage security vulnerabilities.
                 </p>
                 <p>
-                    I graduated from <a href="https://www.purdue.edu" className="about-link">Purdue University</a> in December 2024. I majored in Computer Engineering
-                    and took classes in areas like software engineering, computer security, and data 
-                    science.
+                    I graduated from Purdue in December 2024. After graduation, I started working as a software engineer at Sowfee Health. I am excited to continue my development at Sowfee and learn
+                    from talented engineers at the company.
                 </p>
                 <p>
                     <span className="about-recruitment-status">I am currenly seeking software engineering, data science, and business analyst opportunities.</span>
-                </p>
-                <p>
-                    If you happen to visit the city I reside in (currently Cleveland, Ohio), perhaps we could
-                    meet up. We could:
-                    <ul>
-                        <li>
-                            Share a coffee
-                        </li>
-                        <li>
-                            Discuss coding
-                        </li>
-                        <li>
-                            Plan a day at one of Cleveland's parks
-                        </li>
-                    </ul>
                 </p>
             </section>
         </div>
